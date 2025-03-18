efe89ab7437d18903d440b4a999760e01cf0fcd2
Janki <janki20027@gmail.com>
2025-03-18 20:44:26 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
-int main(){
+
+int main() {
     int N;
-    scanf("%i",&N);
+    scanf("%i", &N);
 
     int arr[N];
-    for(int i=0; i<N; i++){
-        scanf("%i",&arr[i]);
+    for (int i = 0; i < N; i++) {
+        scanf("%i", &arr[i]);
     }
 
     int T;
-    scanf("%i",&T);
+    scanf("%i", &T);
 
-    for(int i=0;i<N;i++){
-        if(i>0 && arr[i]==arr[i-1]){
-            continue;
-        }
-        for(int j=i+1;j<N;j++){
-            if(arr[i]+arr[j]==T){
-                printf("%i %i\n",arr[i],arr[j]);
+    // Sort the array to group identical values together
+    for (int i = 0; i < N - 1; i++) {
+        for (int j = 0; j < N - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        
     }
+
+    // Now, find pairs with sum T
+    for (int i = 0; i < N; i++) {
+        // Skip duplicates in the outer loop
+        if (i > 0 && arr[i] == arr[i - 1]) {
+            continue;
+        }
+        
+        for (int j = i + 1; j < N; j++) {
+            if (arr[i] + arr[j] == T) {
+                printf("%i %i\n", arr[i], arr[j]);
+            }
+        }
+    }
+
     return 0;
 }
