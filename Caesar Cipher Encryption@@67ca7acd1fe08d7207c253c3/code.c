1ea010bc7e801c3a9f566b0ade4da85152a045a2
Janki <janki20027@gmail.com>
2025-03-29 14:33:25 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,16 @@
+void caserCipher(char msg, int shift, char encrypted)
+{
+    *ptr=msg;
+    int length=strlen(ptr);
+    while(ptr[i]!='\0'){
+        if(ptr[i]=' '){
+            printf(" ");
+        }
+        else{
+            for(int i=0; i<length; i++){
+                ptr[i]=ptr[i]+shift;
+                printf("%c",ptr[i]);
+            }
+        }
+    }
+}
